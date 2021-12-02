451e6053c7388353d47a56a367665d199b923fc1
muratcan.bur <muratcanbur@gmail.com>
2021-12-02 16:46:13 +0300

    create Resource.kt

--- core/src/main/java/co/icanteach/apps/cryptocurrencyInfo/Resource.kt
+++ core/src/main/java/co/icanteach/apps/cryptocurrencyInfo/Resource.kt
@@ -8,15 +8,4 @@ sealed class Resource<T> {
 
     data class Loading<T>(val data: T? = null) : Resource<T>()
 
-    fun <R> mapData(transform: (T) -> R): Resource<R> = when (this) {
-        is Success -> Success(
-            transform(data)
-        )
-        is Error -> Error(
-            exception
-        )
-        is Loading -> Loading(
-            data?.let { transform(it) }
-        )
-    }
 }
